d2a5799d5cd9c8f3bb97b59e60e812f3fd29f795
Mica CF <mcohenfuentes@gmail.com>
2025-04-07 15:45:58 -0400

    rendered data in templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('books/', views.book_index, name='book-index'),  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+
+
+class Book:
+    def __init__(self, title, author, genre, description):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.description = description
+
+books = [
+    Book('Pride and Prejudice', 'Jane Austen', 'Fiction', 'A classic. Timeless love story.'),
+    Book('Bunny', 'Mona Awad', 'Fiction', 'Very unreliable narrator.'),
+    Book('Braiding Sweetgrass', 'Robin', 'Nonfiction', 'Beautiful prose about humans relationship with nature.'),
+]
+
+
+def book_index(request):
+    return render(request, 'books/index.html', {'books': books})
 
 def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'books/home.html')
 
 def about(request):
     return render(request, 'about.html')
+
