7223bf1da4d18ab115d1cb00e1d7374ad333cc73
Mica CF <mcohenfuentes@gmail.com>
2025-04-07 17:51:53 -0400

    added classed based views

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book
 
 
@@ -18,3 +19,16 @@ def about(request):
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'books/detail.html', {'book': book})
+
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['description']
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = '/books/'
