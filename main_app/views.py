d935aa1ec70ef287f92b70cef1e9688c41419a7f
Mica CF <mcohenfuentes@gmail.com>
2025-04-07 14:48:23 -0400

    setup, added urls, views and static templates

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello World</h1>')
+
+def about(request):
+    return render(request, 'about.html')
