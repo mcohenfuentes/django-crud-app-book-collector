54ba9d200b0640505643f327931de3c83c057b66
Mica CF <mcohenfuentes@gmail.com>
2025-04-08 11:01:00 -0400

    added authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('books/', views.book_index, name='book-index'),
     path('books/<int:book_id>/', views.book_detail, name='book-detail'),
     path('books/create/', views.BookCreate.as_view(), name='book-create'),
     path('books/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),  
+    path('books/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'), 
+    path('accounts/signup/', views.signup, name='signup'), 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Book
 
 
 
 
-
+@login_required
 def book_index(request):
-    books = Book.objects.all()
+    books = Book.objects.filter(user=request.user)
     return render(request, 'books/index.html', {'books': books})
 
-def home(request):
-    return render(request, 'books/home.html')
+class Home(LoginView):
+    template_name = 'home.html'
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     return render(request, 'books/detail.html', {'book': book})
 
 
-class BookCreate(CreateView):
+class BookCreate(LoginRequiredMixin, CreateView):
     model = Book
     fields = '__all__'
 
-class BookUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user  
+        return super().form_valid(form)
+
+class BookUpdate(LoginRequiredMixin, UpdateView):
     model = Book
     fields = ['description']
 
-class BookDelete(DeleteView):
+class BookDelete(LoginRequiredMixin, DeleteView):
     model = Book
     success_url = '/books/'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
